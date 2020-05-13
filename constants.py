1033471833d2c7d67acf29ed70257f348745ca89
zypang <pangzaiyu@163.com>
2020-05-13 12:31:41 +0800

    refactor firstset codes

--- constants.py
+++ constants.py
@@ -6,5 +6,11 @@ class EBNF_OP(Enum):
     MULTIPLE = 2
     QUESTION = 3
 
+class ELE_TYPE(Enum):
+    TERM=0
+    NONTERM = 1
+    COMBI = 2
+
 EBNF_OP_SYMBOL=['+','*','?']
 EMPTY='#'
+TERM_BEGIN_CHARS= f'"<{EMPTY}'
--- main.py
+++ main.py
@@ -1,9 +1,13 @@
-from constants import EBNF_OP_SYMBOL,EMPTY
+from constants import EBNF_OP_SYMBOL,EMPTY, ELE_TYPE, TERM_BEGIN_CHARS
+from _collections import defaultdict
 import sys
 
 
 global_first_set={}
 global_grammar={}
+
+global_addto_first=defaultdict(list)
+global_addto_follow=defaultdict(list)
 
 class Element:
     """The element of grammar production right part"""
@@ -11,6 +15,12 @@ class Element:
     def __init__(self, symbols, op=''):
         self.symbols = symbols
         self.op = op
+        if self.op != '':
+            self.type= ELE_TYPE.COMBI
+        elif self.symbols[0][0] in TERM_BEGIN_CHARS:
+            self.type = ELE_TYPE.TERM
+        else:
+            self.type = ELE_TYPE.NONTERM
 
     def __str__(self):
         return f"Element({self.symbols} {self.op})"
@@ -49,10 +59,10 @@ def process_right(grammar):
                     symbols = []
                     while item:
                         if item[-1] in EBNF_OP_SYMBOL:
-                            symbols.append(item[:-2])
+                            symbols.append(Element([item[:-2]]))
                             break
                         else:
-                            symbols.append(item)
+                            symbols.append(Element([item]))
                         item = next(line, None)
                     right_elements.append(Element(symbols, item[-1]))
                 else:
@@ -61,59 +71,66 @@ def process_right(grammar):
             new_right.append(right_elements)
         production['right'] = new_right
 
-def nonterminal_first_set(left):
-    productions = global_grammar[left]['right']
+def elements_first_set(elements):
     fset=set()
-    for line in productions:
-        all_empty=True
-        for ele in line:
-            ele_first_set=element_first_set(ele)
-            fset.update(ele_first_set - {EMPTY})
-            if EMPTY not in ele_first_set:
-                all_empty=False
-                break
-        if all_empty:
-            fset.add(EMPTY)
-    return fset
-
-
-def combination_first_set(element):
-    fset = set()
-    all_empty = True
-    for str in element.symbols:
-        str_first_set = string_first_set(str)
-        fset.update(str_first_set - {EMPTY})
-        if EMPTY not in str_first_set:
+    all_empty=True
+    for ele in elements:
+        ele_first_set = element_first_set(ele)
+        fset.update(ele_first_set - {EMPTY})
+        if EMPTY not in ele_first_set:
             all_empty = False
             break
     if all_empty:
         fset.add(EMPTY)
-    if element.op == '?' or element.op == '*':
-        fset.add(EMPTY)
+    return fset
 
+def nonterminal_first_set(left):
+    productions = global_grammar[left]['right']
+    fset=set()
+    for line in productions:
+        fset.update(elements_first_set(line))
     return fset
 
 
-def string_first_set(str):
-    if str[0] in f'"<{EMPTY}':
-        return {str}
-    else:
-        return nonterminal_first_set(str)
+def combination_first_set(element):
+    fset = elements_first_set(element.symbols)
+    if element.op == '?' or element.op == '*':
+        fset.add(EMPTY)
+    return fset
 
+
+#def string_first_set(str):
+#    if str[0] in f'"<{EMPTY}':
+#        return {str}
+#    else:
+#        return nonterminal_first_set(str)
 
 def element_first_set(element):
-    if element.op=='':
+    if element.type==ELE_TYPE.TERM:
+        return {element.symbols[0]}
+    elif element.type == ELE_TYPE.NONTERM:
         str=element.symbols[0]
         if str not in global_first_set:
-            global_first_set[str]=string_first_set(str)
+            global_first_set[str]=nonterminal_first_set(str)
         return global_first_set[str]
-    else:
+    elif element.type == ELE_TYPE.COMBI:
         return combination_first_set(element)
+    else:
+        raise Exception("invalid element type")
 
 def first_set(grammar):
     for nt in grammar:
         if nt not in global_first_set:
             global_first_set[nt]=nonterminal_first_set(nt)
+
+def get_addto_dict(grammar):
+    productions=grammar.values()
+    for prod in productions:
+        for line in prod['right']:
+            for i in range(0,len(line)-1):
+                pass
+            if line[-1].type==ELE_TYPE.COMBI and len(line[-1].symbols)>1:
+                pass
 
 if __name__ == '__main__':
     EBNF_path = "grammar.txt" if len(sys.argv)<=1 else sys.argv[1]
